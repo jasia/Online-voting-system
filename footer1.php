c9c89083ec07e2eedae52add10bfd506784040b7
Jesin Akter <32837539+jasia@users.noreply.github.com>
2025-01-17 15:12:17 +0600

    Add files via upload

---
+++ footer1.php
@@ -0,0 +1,7 @@
+	  <hr>
+
+      <footer>
+        <div class="foot_center2"><p>Copyright &copy; 2025 Bangladesh Open University</p>
+		<p>&nbsp;&nbsp;&nbsp;&nbspProgrammed by:<B>Jesin Akter   Id: 19-0-52-801-030<B></p>
+		</div>
+      </footer>
